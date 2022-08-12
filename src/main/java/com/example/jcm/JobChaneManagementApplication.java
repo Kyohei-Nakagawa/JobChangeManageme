6b170eab9d9cfca36e6d2f4d1fd9e46bc5ef3d2d
Kyohei-Nakagawa <k.nakagawa.0426@gmail.com>
2022-08-12 14:10:40 +0900

    SpringBoot Initializer で作成したプロジェクトを追加

---
+++ src/main/java/com/example/jcm/JobChaneManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.jcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobChaneManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobChaneManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jcm/JobChaneManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobChaneManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
