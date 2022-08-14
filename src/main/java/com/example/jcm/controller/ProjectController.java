da0be34c5a310a86ffaf4d70ec9b2c1cf9ad66fc
Kyohei-Nakagawa <k.nakagawa.0426@gmail.com>
2022-08-14 19:26:27 +0900

    案件登録画面を追加

--- src/main/java/com/example/jcm/controller/ProjectController.java
+++ src/main/java/com/example/jcm/controller/ProjectController.java
@@ -1,12 +1,13 @@
 package com.example.jcm.controller;
 
+import com.example.jcm.controller.form.ProjectForm;
 import com.example.jcm.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/projects")
@@ -21,8 +22,17 @@ public class ProjectController {
         return "projects/list";
     }
 
+    @PostMapping
+    public String addProject(@Validated ProjectForm form, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return showAddForm(form);
+        }
+        projectService.addProject(form.getStatus(), form.getAgent(), form.getCompany(), form.getTitle(), form.getProgramming(), form.getDetail());
+        return "redirect:/projects";
+    }
+
     @GetMapping("/addForm")
-    public String showAddForm() {
+    public String showAddForm(@ModelAttribute ProjectForm form) {
         return "projects/addForm";
     }
 
--- src/main/java/com/example/jcm/repository/ProjectRepository.java
+++ src/main/java/com/example/jcm/repository/ProjectRepository.java
@@ -1,6 +1,7 @@
 package com.example.jcm.repository;
 
 import com.example.jcm.repository.entity.ProjectEntity;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -14,4 +15,8 @@ public interface ProjectRepository {
 
     @Select("select * from projects where id = #{projectId}")
     ProjectEntity findById(long projectId);
+
+    @Insert("insert into projects (status, agent, company, title, programming, detail, uptime, register)\n" +
+            "values (#{status}, #{agent}, #{company}, #{title}, #{programming}, #{detail}, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())")
+    public void addProject(String status, String agent, String company, String title, String programming, String detail);
 }
